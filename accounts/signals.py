594c06048544611edae598f7182147e0ee5667eb
aziz krayyem <azizkrayyem7@gmail.com>
2022-04-10 23:12:07 +0300

    Add Create & Save Profile Signal

--- accounts/signals.py
+++ accounts/signals.py
@@ -8,3 +8,6 @@ def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)# instance=> هاد النسخة الجديدة من اليوز الي رح ينشاْ
 
+@receiver(post_save, sender=User)
+def save_profile(sender,instance, **kwargs):
+    instance.profile.save()
